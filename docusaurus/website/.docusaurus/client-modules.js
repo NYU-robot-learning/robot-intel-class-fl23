ba8f768c2d4a8d2956271d014e090c99dd94aff8
notvenky <notvenky@users.noreply.github.com>
2023-09-05 01:21:40 +0000

    deploy: c6e441ad523e573e29f954f5819e124c768cefd8

--- docusaurus/website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/aadhithya/robot-intel-class-fl23/docusaurus/website/node_modules/infima/dist/css/default/default.css"),
-  require("/home/aadhithya/robot-intel-class-fl23/docusaurus/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/aadhithya/robot-intel-class-fl23/docusaurus/website/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/home/aadhithya/robot-intel-class-fl23/docusaurus/website/src/css/custom.css"),
-];
